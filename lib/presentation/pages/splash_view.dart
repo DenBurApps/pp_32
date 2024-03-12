9975cfece194ca447e89bb1c65b8767a6e9e3830
juiceNewton-1 <juicenewtoncallmeangel@gmail.com>
2024-03-12 00:23:09 -0700

    Update app

--- lib/presentation/pages/splash_view.dart
+++ lib/presentation/pages/splash_view.dart
@@ -1,8 +1,6 @@
 import 'dart:async';
 import 'dart:developer';
-
 import 'package:connectivity_plus/connectivity_plus.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
